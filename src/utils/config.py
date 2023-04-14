b7ffbee2a8f0370bb814e3720c0b6d05d8c453a4
cauzpema@gmail.com <cauzpema@gmail.com>
2023-04-14 21:26:44 +0200

    Add Config class

---
+++ src/utils/config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+
+class Config:
+    """
+    Holds configuration parameters
+    """
+
+    def __init__(self):
+        self.name = "basic_unet"
+        self.root_dir = (
+            Path(__file__)
+            .resolve()
+            .parents[2]
+            .joinpath("data")
+            .joinpath("output")
+            .joinpath("cleaned_data")
+        )
+        self.n_epochs = 10
+        self.learning_rate = 0.0002
+        self.batch_size = 32
+        self.patch_size = 64
+        self.test_results_dir = (
+            Path(__file__)
+            .resolve()
+            .parents[2]
+            .joinpath("data")
+            .joinpath("output")
+            .joinpath("test_results")
+        )
