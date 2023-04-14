6b28dd4e9556047d9fbfab1a21d0772a9ea85cdc
cauzpema@gmail.com <cauzpema@gmail.com>
2023-04-14 21:34:04 +0200

    Complete UNetExperiment class, more testing pending

--- src/experiments/UNetExperiment.py
+++ src/experiments/UNetExperiment.py
@@ -15,15 +15,17 @@ from torch.utils.tensorboard import SummaryWriter
 from data_prep.SlicesDataset import SlicesDataset
 from inference.UNetInferenceAgent import UNetInferenceAgent
 from networks.RecursiveUNet import UNet
+from utils.config import Config
 from utils.utils import log_to_tensorboard
-from utils.volume_stats import Dice3d, Jaccard3d
+from utils.volume_stats import dice_3d, jaccard_3d, sensitivity
 
 
 class UNetExperiment:
     """
-    This class implements the basic life cycle for a segmentation task with UNet(https://arxiv.org/abs/1505.04597).
-    The basic life cycle of a UNetExperiment is:
+    This class implements the basic life cycle for a segmentation task with UNet
+        (https://arxiv.org/abs/1505.04597).
 
+    The basic life cycle of a UNetExperiment is:
         run():
             for epoch in n_epochs:
                 train()
@@ -31,7 +33,7 @@ class UNetExperiment:
         test()
     """
 
-    def __init__(self, config, split, dataset):
+    def __init__(self, config: Config, split, dataset):
         self.n_epochs = config.n_epochs
         self.split = split
         self._time_start = ""
@@ -39,15 +41,13 @@ class UNetExperiment:
         self.epoch = 0
         self.name = config.name
 
-        # Create output folders
-        dirname = f'{time.strftime("%Y-%m-%d_%H%M", time.gmtime())}_{self.name}'
-        self.out_dir = os.path.join(config.test_results_dir, dirname)
-        os.makedirs(self.out_dir, exist_ok=True)
+        # 1. Create output folders
+        self.out_dir = config.test_results_dir.joinpath(
+            f'{time.strftime("%Y-%m-%d_%H%M", time.gmtime())}_{self.name}'
+        )
+        self.out_dir.mkdir(exist_ok=True, parents=True)
 
-        # Create data loaders
-        # TASK: SlicesDataset class is not complete. Go to the file and complete it.
-        # Note that we are using a 2D version of UNet here, which means that it will expect
-        # batches of 2D slices.
+        # 2. Create data loaders
         self.train_loader = DataLoader(
             SlicesDataset(dataset[split["train"]]),
             batch_size=config.batch_size,
@@ -61,20 +61,17 @@ class UNetExperiment:
             num_workers=0,
         )
 
-        # we will access volumes directly for testing
+        # 3. We will access volumes directly for testing
         self.test_data = dataset[split["test"]]
 
-        # Do we have CUDA available?
+        # 4. Do we have CUDA available?
         if not torch.cuda.is_available():
             print(
                 "WARNING: No CUDA device is found. This may take significantly longer!"
             )
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # Configure our model and other training implements
-        # We will use a recursive UNet model from German Cancer Research Center,
-        # Division of Medical Image Computing. It is quite complicated and works
-        # very well on this task. Feel free to explore it or plug in your own model
+        # 5. Configure our model and other training implements
         self.model = UNet(num_classes=3)
         self.model.to(self.device)
 
@@ -85,12 +82,19 @@ class UNetExperiment:
 
         # We are using standard SGD method to optimize our weights
         self.optimizer = optim.Adam(self.model.parameters(), lr=config.learning_rate)
+
         # Scheduler helps us update learning rate automatically
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, "min")
 
-        # Set up Tensorboard. By default it saves data into runs folder. You need to launch
-        self.tensorboard_train_writer = SummaryWriter(comment="_train")
-        self.tensorboard_val_writer = SummaryWriter(comment="_val")
+        # Set up Tensorboard. By default it saves data into runs folder.
+        tensorboard_path = config.test_results_dir.joinpath("runs")
+        tensorboard_path.mkdir(exist_ok=True, parents=True)
+        self.tensorboard_train_writer = SummaryWriter(
+            log_dir=tensorboard_path, comment="_train"
+        )
+        self.tensorboard_val_writer = SummaryWriter(
+            log_dir=tensorboard_path, comment="_val"
+        )
 
     def train(self):
         """
@@ -100,27 +104,24 @@ class UNetExperiment:
         print(f"Training epoch {self.epoch}...")
         self.model.train()
 
-        # Loop over our minibatches
+        # 1. Loop over training minibatches
         for i, batch in enumerate(self.train_loader):
             self.optimizer.zero_grad()
 
-            # TASK: You have your data in batch variable. Put the slices as 4D Torch Tensors of
-            # shape [BATCH_SIZE, 1, PATCH_SIZE, PATCH_SIZE] into variables data and target.
-            # Feed data to the model and feed target to the loss function
-            #
-            # data = <YOUR CODE HERE>
-            # target = <YOUR CODE HERE>
+            # 1.1. Get data and target batches
+            data = batch["image"].float().to(device=self.device)
+            target = batch["seg"].to(device=self.device)
 
+            # 1.2. Get prediction loss
             prediction = self.model(data)
 
-            # We are also getting softmax'd version of prediction to output a probability map
-            # so that we can see how the model converges to the solution
+            # We are also getting softmax'd version of prediction to output a
+            # probability map so that we can see how the model converges to the
+            # solution.
+            # We predict class probabilities (one per class) for each voxel in the image.
             prediction_softmax = F.softmax(prediction, dim=1)
 
             loss = self.loss_function(prediction, target[:, 0, :, :])
-
-            # TASK: What does each dimension of variable prediction represent?
-            # ANSWER:
 
             loss.backward()
             self.optimizer.step()
@@ -128,14 +129,12 @@ class UNetExperiment:
             if (i % 10) == 0:
                 # Output to console on every 10th batch
                 print(
-                    f"\nEpoch: {self.epoch} Train loss: {loss}, {100*(i+1)/len(self.train_loader):.1f}% complete"
+                    f"\nEpoch: {self.epoch} Train loss: {loss},"
+                    f" {100*(i+1)/len(self.train_loader):.1f}% complete"
                 )
 
                 counter = 100 * self.epoch + 100 * (i / len(self.train_loader))
 
-                # You don't need to do anything with this function, but you are welcome to
-                # check it out if you want to see how images are logged to Tensorboard
-                # or if you want to output additional debug data
                 log_to_tensorboard(
                     self.tensorboard_train_writer,
                     loss,
@@ -163,12 +162,19 @@ class UNetExperiment:
         self.model.eval()
         loss_list = []
 
+        # 1. Loop through validation minibatches
         with torch.no_grad():
             for i, batch in enumerate(self.val_loader):
-                # TASK: Write validation code that will compute loss on a validation sample
-                # <YOUR CODE HERE>
+                # 1.1. Get data and target batches
+                data = batch["image"].float().to(device=self.device)
+                target = batch["seg"].to(device=self.device)
 
-                print(f"Batch {i}. Data shape {data.shape} Loss {loss}")
+                # 1.2. Get prediction loss
+                prediction = self.model(data)
+                prediction_softmax = F.softmax(prediction, dim=1)
+                loss = self.loss_function(prediction, target[:, 0, :, :])
+
+                print(f"Batch {i}. Data shape {data.shape}. Loss {loss}")
 
                 # We report loss that is accumulated across all of validation set
                 loss_list.append(loss.item())
@@ -211,63 +217,48 @@ class UNetExperiment:
 
     def run_test(self):
         """
-        This runs test cycle on the test dataset.
-        Note that process and evaluations are quite different
-        Here we are computing a lot more metrics and returning
-        a dictionary that could later be persisted as JSON
+        This runs test cycle on the test dataset. Note that process and evaluations
+            are quite different. Here we are computing a lot more metrics and returning
+            a dictionary that could later be persisted as JSON.
+
+        In this method we will be computing metrics that are relevant to the task of 3D
+            volume segmentation. Therefore, unlike train and validation methods, we will
+            do inferences on full 3D volumes, much like we will be doing it when we
+            deploy the model in the clinical environment.
         """
         print("Testing...")
         self.model.eval()
 
-        # In this method we will be computing metrics that are relevant to the task of 3D volume
-        # segmentation. Therefore, unlike train and validation methods, we will do inferences
-        # on full 3D volumes, much like we will be doing it when we deploy the model in the
-        # clinical environment.
-
-        # TASK: Inference Agent is not complete. Go and finish it. Feel free to test the class
-        # in a module of your own by running it against one of the data samples
         inference_agent = UNetInferenceAgent(model=self.model, device=self.device)
 
         out_dict = {}
         out_dict["volume_stats"] = []
-        dc_list = []
-        jc_list = []
 
-        # for every in test set
+        # 1. Compute metrics for every volume in the test set
         for i, x in enumerate(self.test_data):
             pred_label = inference_agent.single_volume_inference(x["image"])
 
-            # We compute and report Dice and Jaccard similarity coefficients which
-            # assess how close our volumes are to each other
-
-            # TASK: Dice3D and Jaccard3D functions are not implemented.
-            #  Complete the implementation as we discussed
-            # in one of the course lessons, you can look up definition of Jaccard index
-            # on Wikipedia. If you completed it
-            # correctly (and if you picked your train/val/test split right ;)),
-            # your average Jaccard on your test set should be around 0.80
-
-            dc = Dice3d(pred_label, x["seg"])
-            jc = Jaccard3d(pred_label, x["seg"])
-            dc_list.append(dc)
-            jc_list.append(jc)
-
-            # STAND-OUT SUGGESTION: By way of exercise, consider also outputting:
-            # * Sensitivity and specificity (and explain semantic meaning in terms of
-            #   under/over segmenting)
-            # * Dice-per-slice and render combined slices with lowest and highest DpS
-            # * Dice per class (anterior/posterior)
-
+            # 1.1. Compute and report performance metrics
             out_dict["volume_stats"].append(
-                {"filename": x["filename"], "dice": dc, "jaccard": jc}
+                {
+                    "filename": x["filename"],
+                    "dice": dice_3d(x["seg"], pred_label),
+                    "jaccard": jaccard_3d(x["seg"], pred_label),
+                    "tpr": sensitivity(x["seg"], pred_label),
+                    "tnr": sensitivity(x["seg"], pred_label),
+                }
             )
             print(
-                f"{x['filename']} Dice {dc:.4f}. {100*(i+1)/len(self.test_data):.2f}% complete"
+                f"[{100*(i+1)/len(self.test_data):.2f}% complete] "
+                f"{x['filename']} Dice {out_dict['volume_stats']['dice']:.4f}."
             )
 
+        # 2. Compute metrics' averages over all test instances
         out_dict["overall"] = {
-            "mean_dice": np.mean(dc_list),
-            "mean_jaccard": np.mean(jc_list),
+            "mean_dice": np.mean([e["dice"] for e in out_dict["volume_stats"]]),
+            "mean_jaccard": np.mean([e["jaccard"] for e in out_dict["volume_stats"]]),
+            "mean_tpr": np.mean([e["tpr"] for e in out_dict["volume_stats"]]),
+            "mean_tnr": np.mean([e["tnr"] for e in out_dict["volume_stats"]]),
         }
 
         print("\nTesting complete.")
@@ -291,5 +282,6 @@ class UNetExperiment:
 
         self._time_end = time.time()
         print(
-            f"Run complete. Total time: {time.strftime('%H:%M:%S', time.gmtime(self._time_end - self._time_start))}"
+            "Run complete. Total time:"
+            f" {time.strftime('%H:%M:%S', time.gmtime(self._time_end - self._time_start))}"
         )
