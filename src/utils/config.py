d7bfc20a29e2960ef49fbc94cadaa0e67f969199
cauzpema@gmail.com <cauzpema@gmail.com>
2023-04-16 08:44:24 +0200

    Update experiment configuration

--- src/utils/config.py
+++ src/utils/config.py
@@ -14,11 +14,12 @@ class Config:
             .parents[2]
             .joinpath("data")
             .joinpath("output")
+            .joinpath("section_1")
             .joinpath("cleaned_data")
         )
         self.n_epochs = 50
-        self.learning_rate = 0.0002
-        self.batch_size = 32
+        self.learning_rate = 0.0001
+        self.batch_size = 64
         self.patch_size = 64
         self.test_results_dir = (
             Path(__file__)
@@ -26,5 +27,6 @@ class Config:
             .parents[2]
             .joinpath("data")
             .joinpath("output")
+            .joinpath("section_2")
             .joinpath("test_results")
         )
